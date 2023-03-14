4fbe2a830083beca5683fa5b28dd15c902938355
Andie.Phan <andie.phan@analog.com>
2023-03-14 16:50:04 -0700

    Component moved to @otosense/ogui

--- src/testData.ts
+++ src/testData.ts
@@ -1,31 +1,11 @@
-import { type Operator, type Optional, type Session } from './utility'
-
-export interface SessionFilterOptions {
-  device_ids: Optional<string[]>
-  from_bt: Optional<number>
-  to_bt: Optional<number>
-  from_tt: Optional<number>
-  to_tt: Optional<number>
-  annotations?: Optional<{
-    names: string[]
-    operator: Operator
-  }>
-  channels: Optional<{
-    names: string[]
-    operator: Operator
-  }>
-  sr: Optional<number>
-}
-
-export interface SessionSortOptions {
-  field: 'device_id' | 'bt' | 'tt' | 'sr'
-  mode: 'asc' | 'desc'
-}
-
-export interface PaginationOptions {
-  from_idx: number
-  to_idx: number
-}
+import {
+  type Operator,
+  type Optional,
+  type PaginationOptions,
+  type Session,
+  type SessionFilterOptions,
+  type SessionSortOptions
+} from '@otosense/ogui/lib/SessionTable/types'
 
 const mockAnnotations = (start: number, stop: number, step: number, tag: number): Session['annotations'] => {
   const annotations = []
