8a75106f91030979c3f721cc3a53d919f0aac8b2
Andie.Phan <andie.phan@analog.com>
2023-03-17 18:07:04 -0700

    add setComponentValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './App.css'
 import { mockListSessions } from './testData'
 import { OtoTable } from '@otosense/ogui'
+import { type Session } from '@otosense/ogui/lib/SessionTable/types'
 
-const App = (): JSX.Element => <OtoTable listSessions={mockListSessions}/>
+const App = (): JSX.Element => {
+  const [data, setData] = useState<Session[]>([])
+  useEffect(() => {
+    setData(mockListSessions())
+  }, [])
+
+  return (
+    <>
+      <OtoTable
+        data={data}
+        setComponentValue={(v) => {
+          setData(mockListSessions(v.filter, v.sort, v.pagination))
+        }}
+      />
+    </>
+  )
+}
 
 export default App
